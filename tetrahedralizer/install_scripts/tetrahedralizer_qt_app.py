5e40ae94409870af8a420397c5b1cffd741bbe5e
Andrew <andrew.s.creegan@gmail.com>
2022-03-23 11:38:27 +1300

    actually os independent paths this time

--- tetrahedralizer/install_scripts/tetrahedralizer_qt_app.py
+++ tetrahedralizer/install_scripts/tetrahedralizer_qt_app.py
@@ -10,16 +10,16 @@ def install():
     """
     Installation script for tetrahedralizer_qt.py. Bundles the app with pyinstaller, then tidies things up.
     """
-    package_dir = str(Path(tetrahedralizer.__file__).parent)
+    package_dir = os.path.dirname(tetrahedralizer.__file__)
     working_dir = os.getcwd()
-    workpath = str(Path(working_dir + r"\build"))
-    distpath = str(Path(working_dir + r"\tetrahedralizer_qt"))
+    workpath = os.path.join(working_dir, "build")
+    distpath = os.path.join(working_dir, "tetrahedralizer_qt")
     specpath = working_dir
 
-    app_path = str(Path(package_dir + r"\app\tetrahedralizer_qt_app\tetrahedralizer_qt.py"))
-    layout_path = str(Path(package_dir + r"\app\tetrahedralizer_qt_app\layout\tetrahedralizer_layout.ui"))
-    conf_path = str(Path(package_dir + r"\app\tetrahedralizer_qt_app\conf.json"))
-    hooks_dir = str(Path(package_dir + r"\install_scripts"))
+    app_path = os.path.join(package_dir, "app", "tetrahedralizer_qt_app", "tetrahedralizer_qt.py")
+    layout_path = os.path.join(package_dir, "app", "tetrahedralizer_qt_app", "layout", "tetrahedralizer_layout.ui")
+    conf_path = os.path.join(package_dir, "app", "tetrahedralizer_qt_app", "conf.json")
+    hooks_dir = os.path.join(package_dir, "install_scripts")
 
     # Remove the distribution directory manually, since pyinstaller doesn't know about everything we put there
     if os.path.exists(distpath):
@@ -41,20 +41,20 @@ def install():
     ])
 
     # Move conf.json up one level to get it out of the mess (as matched in tetrahedralizer_qt.py)
-    shutil.move(distpath + r"\tetrahedralizer_qt\conf.json", distpath)
+    shutil.move(os.path.join(distpath, "tetrahedralizer_qt", "conf.json"), distpath)
 
     # Get the example meshes
-    shutil.copytree(package_dir + r"\app\example_meshes", distpath + r"\example_meshes")
-    os.remove(distpath + r"\example_meshes\__init__.py")
+    shutil.copytree(os.path.join(package_dir, "app", "example_meshes"), os.path.join(distpath, "example_meshes"))
+    os.remove(os.path.join(distpath, "example_meshes", "__init__.py"))
 
     # Create a shortcut to the pyinstaller exe and place it one level up
     short = make_shortcut(" ", name="tetrahedralizer_qt", terminal=False, startmenu=False,
-                      executable=distpath + r"\tetrahedralizer_qt\tetrahedralizer_qt.exe")
-    shutil.move(Path(short.desktop_dir + "\\" + short.target), distpath)
+                      executable=os.path.join(distpath, "tetrahedralizer_qt","tetrahedralizer_qt.exe"))
+    shutil.move(os.path.join(short.desktop_dir, short.target), distpath)
 
     # Get rid of pyinstaller working files
     shutil.rmtree(workpath)
-    os.remove(specpath + r"\tetrahedralizer_qt.spec")
+    os.remove(os.path.join(specpath,"tetrahedralizer_qt.spec"))
 
 
 if __name__ == "__main__":
